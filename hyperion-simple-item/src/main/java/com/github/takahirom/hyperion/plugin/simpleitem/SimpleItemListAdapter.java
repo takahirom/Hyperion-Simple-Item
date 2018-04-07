6809ffb159eda4a1618ebccf3c2647f6d727ddef
takahirom <takam.dev@gmail.com>
2018-04-07 21:58:53 +0900

    Delete ListAdapter for backward compatibility

--- hyperion-simple-item/src/main/java/com/github/takahirom/hyperion/plugin/simpleitem/SimpleItemListAdapter.java
+++ hyperion-simple-item/src/main/java/com/github/takahirom/hyperion/plugin/simpleitem/SimpleItemListAdapter.java
@@ -1,35 +1,24 @@
 package com.github.takahirom.hyperion.plugin.simpleitem;
 
 import android.support.annotation.NonNull;
-import android.support.v7.recyclerview.extensions.ListAdapter;
-import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 import takahirom.github.com.hyperion_simple_item.R;
 
 class SimpleItemListAdapter
-    extends ListAdapter<SimpleItem, SimpleItemListAdapter.SimpleItemViewHolder> {
+    extends RecyclerView.Adapter<SimpleItemListAdapter.SimpleItemViewHolder> {
 
-  private static final DiffUtil.ItemCallback<SimpleItem> DIFF_CALLBACK =
-      new DiffUtil.ItemCallback<SimpleItem>() {
-        @Override
-        public boolean areItemsTheSame(@NonNull SimpleItem oldUser, @NonNull SimpleItem newUser) {
-          return oldUser.equals(newUser);
-        }
-
-        @Override public boolean areContentsTheSame(@NonNull SimpleItem oldUser,
-            @NonNull SimpleItem newUser) {
-          return oldUser.equals(newUser);
-        }
-      };
   private LayoutInflater layoutInflater;
+  @NonNull private List<SimpleItem> items = new ArrayList<SimpleItem>();
 
   SimpleItemListAdapter(LayoutInflater layoutInflater) {
-    super(DIFF_CALLBACK);
+    super();
     this.layoutInflater = layoutInflater;
   }
 
@@ -40,13 +29,22 @@ class SimpleItemListAdapter
   }
 
   @Override public void onBindViewHolder(@NonNull final SimpleItemViewHolder holder, int position) {
-    final SimpleItem item = getItem(position);
+    final SimpleItem item = items.get(position);
     holder.title.setText(item.title);
     holder.text.setText(item.text);
     holder.text.setVisibility(item.text == null ? View.GONE : View.VISIBLE);
     holder.image.setImageResource(item.image);
     holder.image.setVisibility(item.image == 0 ? View.GONE : View.VISIBLE);
     holder.itemView.setOnClickListener(item.clickListener);
+  }
+
+  @Override public int getItemCount() {
+    return items.size();
+  }
+
+  public void submitList(List<SimpleItem> items){
+    this.items = items;
+    notifyDataSetChanged();
   }
 
   class SimpleItemViewHolder extends RecyclerView.ViewHolder {
